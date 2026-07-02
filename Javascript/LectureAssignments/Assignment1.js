79b716b6cedc1e66fb6d6b5f78fdfc38f654973c
ManasMishra09 <sanamormanas1999@gmail.com>
2026-07-02 13:52:36 +0530

    Height,color,Pizza

--- Javascript/LectureAssignments/Assignment1.js
+++ Javascript/LectureAssignments/Assignment1.js
@@ -1,6 +1,9 @@
-Steps to initialize git and connecting vs code are :
-1. git init
-2. Copy the .git link and paste in the terminal as - 
-     git remote add origin https://github.com/ManasMishra09/Full-Stack.git
-3. git add.
-4. git commit -m "First Commit"
+// Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+
+const favoriteColor = "blue";
+let heightInCentimeters = 175;
+var likesPizza = true;
+
+console.log("Favorite Color:", favoriteColor);
+console.log("Height in Centimeters:", heightInCentimeters);
+console.log("Likes Pizza:", likesPizza);
