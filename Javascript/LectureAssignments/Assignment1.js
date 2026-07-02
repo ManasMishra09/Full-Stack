b134f582438b77844594eea2c897b65002d5190f
ManasMishra09 <sanamormanas1999@gmail.com>
2026-07-02 14:37:02 +0530

    Odd or Even

--- Javascript/LectureAssignments/Assignment1.js
+++ Javascript/LectureAssignments/Assignment1.js
@@ -1,13 +1,8 @@
-// Write a function called canVote that returns true or false if the age of a user is > 18
+// Write an if/else statement that checks if a number is even or odd. If it's even, print "The number is even." Otherwise, print "The number is odd."
+let number = 7;
 
-function canVote(age) {
-    if (age >= 18) {
-        return true;
-    }
-    else {
-        return false;
-    }
+if (number % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
 }
-
-let age = 20;
-console.log(canVote(age)); // Output: true
