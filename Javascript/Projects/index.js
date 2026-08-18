57e32054b8043130597429800c94f3b2df513e5c
ManasMishra09 <sanamormanas1999@gmail.com>
2026-08-18 16:56:22 +0530

    Axios vs fetch

--- Javascript/Projects/index.js
+++ Javascript/Projects/index.js
@@ -1,39 +1,23 @@
 const express = require('express');
 const app = express();
-let requestCount = 0;
-
-
-function middleware (req,res,next){
-   requestCount++;
-   next();
+const axios = require('axios');
+app.use(express.json());
+// Post using fetch
+async function main(){
+   const response = await fetch("https://www.toptal.com/developers/postbin/1706261117587-5522551864851",
+      {
+      method : "POST",
+   },
+);
+   const textualData = await response.text();
+   console.log(textualData);
 }
 
-app.get("/requestCount", function(req,res){
-   res.send({
-      requestCount
-   })
-})
 
-app.use(middleware); //globslly defined; Worked for all the below routes.
-app.use(express.json());
-//middlewares
-
-
-app.get("/multiply", function(req,res){
-   const a = parseInt(req.query.a);
-   const b = parseInt(req.query.b);
-
-   const ans = a * b;
-   res.json({
-      ans : ans
-   })
-})
-
-app.get("/status",function(req,res){
-   res.send("up")
-})
-
-
-
-
+//Post using axios
+async function main(){
+   const response = await axios.post("https://www.toptal.com/developers/postbin/1706261117587-5522551864851");
+   console.log(response.data);
+}
+main();
 app.listen(3000);
